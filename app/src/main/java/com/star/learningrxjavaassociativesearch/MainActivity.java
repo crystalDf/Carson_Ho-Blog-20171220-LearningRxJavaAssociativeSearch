db3b91682131fbd4145ce06e30d69bfc624eccee
crystalDf <chendong333@gmail.com>
2018-06-29 21:25:28 +0800

    LearningRxJavaAssociativeSearch

---
+++ app/src/main/java/com/star/learningrxjavaassociativesearch/MainActivity.java
@@ -0,0 +1,59 @@
+package com.star.learningrxjavaassociativesearch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "RxJava";
+
+    private EditText mEditText;
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEditText = findViewById(R.id.edit_text);
+        mTextView = findViewById(R.id.text_view);
+
+        RxTextView
+                .textChanges(mEditText)
+                .debounce(1, TimeUnit.SECONDS)
+                .skip(1)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<CharSequence>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Log.d(TAG, "开始采用subscribe连接");
+                    }
+
+                    @Override
+                    public void onNext(CharSequence charSequence) {
+                        mTextView.setText("发送给服务器的字符 = " + charSequence.toString());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "对Error事件作出响应 " + e.toString());
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.d(TAG, "对Complete事件作出响应");
+                    }
+                });
+    }
+}
